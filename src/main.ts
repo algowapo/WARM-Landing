187dbf63b79a7664d73baa1169f744c09856738b
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-04-27 00:21:30 -0500

    ⚡️Initial Project Configuration
    Add key dependencies, favicon, fonts, and metadata.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSmoothScroll from 'vue3-smooth-scroll'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueSmoothScroll)
+app.mount('#app')
