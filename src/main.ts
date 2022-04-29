a184a6a540d0b5cd5f938bc60c19bbd3adf6b7a1
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-04-29 11:35:18 -0500

    ⬆️ Add Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueSmoothScroll from 'vue3-smooth-scroll'
+import './index.css'
 
 const app = createApp(App)
 app.use(VueSmoothScroll)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'light-black': '#0B0B0F',
+				'dark-gray': '#1f2029',
+				'medium-gray': '#424457',
+				'light-gray': '#CDCED9',
+			},
+		},
+	},
+	plugins: [],
+}
