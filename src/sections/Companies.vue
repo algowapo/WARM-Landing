d6f41a3c701c509875e35163132f3aa153cd5b31
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-05-01 11:39:02 -0500

    ✨Finish Companies

--- src/sections/Companies.vue
+++ src/sections/Companies.vue
@@ -1,37 +1,42 @@
 <template>
-	<section id="icons">
-		<p class="icons-text">
-			Las redes de utilidades más grandes del Perú son compatibles con
-		</p>
-		<div class="icons-row">
-			<img src="../assets/sedapal.png" alt="Sedapal Logo" />
-			<img src="../assets/Luz_del_Sur.png" alt="Luz del Sur Logo" />
-			<img src="../assets/Enel_Group_logo.png" alt="Enel Logo" />
-			<img src="../assets/kallpa.png" alt="Kallpa Logo" />
-			<img src="../assets/enosa.png" alt="Enosa Logo" />
+	<section id="icons" class="py-10">
+		<div
+			class="mb-20 flex justify-center flex-col lg:flex-row lg:justify-start lg:space-x-4"
+		>
+			<p class="font-medium">
+				Las redes de utilidades más grandes del Perú son compatibles con:
+			</p>
+			<BrandIcon />
+		</div>
+		<div
+			class="flex space-y-20 items-center flex-col lg:flex-row lg:justify-center lg:space-x-20 lg:space-y-0 mt-10"
+		>
+			<img
+				class="h-16 lg:h-12"
+				src="../assets/sedapal.png"
+				alt="Sedapal Logo"
+			/>
+			<img
+				class="h-16 lg:h-12"
+				src="../assets/Luz_del_Sur.png"
+				alt="Luz del Sur Logo"
+			/>
+			<img
+				class="h-16 md:h-12"
+				src="../assets/Enel_Group_logo.png"
+				alt="Enel Logo"
+			/>
+			<img class="h-16 md:h-12" src="../assets/kallpa.png" alt="Kallpa Logo" />
+			<img class="h-16 md:h-12" src="../assets/enosa.png" alt="Enosa Logo" />
 		</div>
 	</section>
 </template>
 
 <script>
-export default {}
+import BrandIcon from '../components/BrandIcon.vue'
+export default {
+	components: { BrandIcon },
+}
 </script>
 
-<style scoped>
-#icons {
-	padding: 1vh 0;
-	padding-top: 5vh;
-	padding-bottom: 5vh;
-}
-
-.icons-text {
-	font-weight: 400;
-	margin-bottom: 5vh;
-}
-
-.icons-row {
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
-}
-</style>
+<style scoped></style>
