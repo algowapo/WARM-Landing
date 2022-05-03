30cbba4ca2b3c68e56ca7e365caa760d89c004c3
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-05-03 11:43:42 -0500

    🔨 Refactored Home

--- src/sections/Home.vue
+++ src/sections/Home.vue
@@ -1,24 +1,29 @@
 <template>
-	<section id="home">
-		<div class="box-top">
+	<section id="home" class="min-h-[70vh] pt-14 pb-20">
+		<div class="flex items-center">
 			<span class="material-icons material-symbols-outlined"> payments </span>
-			<p class="home-top">Ahorra en tus recibos mensuales</p>
+			<p class="text-lg font-medium ml-2">Ahorra en tus recibos mensuales</p>
 		</div>
-		<div class="box-title">
-			<h1 class="home-title">El consumo de tus utilidades en tus manos</h1>
+		<div class="w-full lg:w-1/2 my-10">
+			<h1 class="text-6xl sm:text-7xl font-semibold">
+				El consumo de tus utilidades en tus manos
+			</h1>
 		</div>
-		<div class="box-subtitle">
-			<p class="home-subtitle">
+		<div class="w-full lg:w-1/3 my-10">
+			<p class="text-lg text-[#d2d3d4] font-normal">
 				La única aplicación que necesita para realizar un seguimiento de los
 				recursos en su hogar. El uso de luz, agua y gas nunca fue tan fácil de
 				gestionar.
 			</p>
 		</div>
-
-		<div class="cta-row">
+		<div
+			class="flex flex-col sm:flex-row items-start space-y-8 sm:space-x-8 sm:space-y-0 mt-20 sm:mt-14"
+		>
 			<MainButton text="Inicia Ahora" />
-			<p class="bottom-text">Como funciona</p>
-			<IconButton icon-name="play_arrow" />
+			<div class="flex items-center space-x-4">
+				<p class="text-lg">Como funciona</p>
+				<IconButton icon-name="play_arrow" />
+			</div>
 		</div>
 	</section>
 </template>
@@ -31,58 +36,4 @@ export default {
 }
 </script>
 
-<style scoped>
-#home {
-	min-height: 40vh;
-	padding: 1vh 0;
-	margin-bottom: 5vh;
-}
-
-.box-top {
-	display: flex;
-	align-items: center;
-	margin-top: 2rem;
-}
-
-.home-top {
-	font-size: 1.1rem;
-	font-weight: 400;
-	margin-left: 1rem;
-}
-
-.box-title {
-	width: 700px;
-	margin-top: 1rem;
-}
-
-.home-title {
-	font-size: calc(60px + (80 - 60) * ((100vw - 300px) / (1600 - 300)));
-	font-weight: 600;
-	line-height: 11vh;
-}
-
-.box-subtitle {
-	width: 550px;
-	margin-top: 3rem;
-}
-
-.home-subtitle {
-	font-size: 1.2rem;
-	color: #d2d3d4;
-	font-weight: 400;
-}
-
-.cta-row {
-	display: flex;
-	align-items: center;
-	margin-top: 4rem;
-}
-
-.cta-row > * {
-	margin-right: 2vw;
-}
-
-.bottom-text {
-	font-size: 1.2rem;
-}
-</style>
+<style scoped></style>
